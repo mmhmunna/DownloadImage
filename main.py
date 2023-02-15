9f43fbbb56b8b1217bfd664f0d5573b7a1e14062
mmhmunna <mhmunna.me@gmail.com>
2023-02-15 14:56:50 +0600

    Full script of download images from a webpage

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
+import time
+from urllib.parse import urlparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import urllib.request
+from datetime import datetime
 
-'''
+
+                ######### For headless   #########
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -12,24 +16,58 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome((ChromeDriverManager().install()), options=chrome_options)
-'''
-driver = webdriver.Chrome(ChromeDriverManager().install())
+
+                ######### For not headless  #########
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+print("Uh, hold on a sec. Once we finish downloading those images, we'll show you the directory they're saved in. Thanks for your patience.")
 driver.maximize_window()
-baseUrl = input("Input Url from which page you want to downlaod photo:")
-if (baseUrl[0:8] == "https://"):
-    baseurl = baseUrl
-else:
-    baseurl = "https://"+baseUrl
+driver.implicitly_wait(5)
+
+                ######### For Dynamic input by terminal  #########
+#baseUrl = input("Input Url from which page you want to downlaod photo:")
+
+                ######### For Static input by terminal  #########
+baseUrl = "https://www.munna.cyou/"
 driver.get(baseUrl)
-title = driver.title
-imageTag = driver.find_elements(By.TAG_NAME,'img')
+
+                ######### Check www  #########
+
+if("www" in baseUrl):
+    domainNameParse = urlparse(baseUrl).hostname
+    domainName = domainNameParse.split('.')[1]
+else:
+    domainNameParse = urlparse(baseUrl).hostname
+    domainName = domainNameParse.split('.')[0]
+
+
+                ######### For collect Current datetime  #########
+dateTime = datetime.now().strftime("%Y%m%d-%H%M%S")
+directoryName = domainName + dateTime
+os.mkdir("Photos" + "/" + directoryName)
+
+imageTag = []
+
+                ######### Scroll Top to bottom   #########
+height = driver.execute_script("return document.body.scrollHeight")
+scrollSpeed = 1
+scrollPauseTime = 0
+while scrollSpeed > 0:
+    scrollSpeed -= 1
+    for x in range(0, height, 5):
+        driver.execute_script(f"window.scrollTo(0, {x});")
+        imageTag.extend(driver.find_elements(By.TAG_NAME, 'img'))
+        time.sleep(scrollPauseTime)
+
+imageTag = set(imageTag)
 i = 1
 for images in imageTag:
     imageSrc = images.get_attribute('src')
     try:
-        urllib.request.urlretrieve(imageSrc,("Photos" + '/' + title +"/" + "photo"+ str(i) +".png" ))
+        urllib.request.urlretrieve(imageSrc,( "Photos" + "/" + directoryName + "/" + domainName + str(i) +".png" ))
         i = i+1
     except:
         continue
+print("Images are here: Photos/"+directoryName)
+
 
 
